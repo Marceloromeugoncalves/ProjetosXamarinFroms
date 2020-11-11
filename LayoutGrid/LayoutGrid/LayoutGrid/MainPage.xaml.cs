832f2c60e33685a60ff938af1e9dd3771e1ea13f
Marceloromeugoncalves <marceloromeugoncalves@gmail.com>
2020-11-11 20:23:36 -0300

    Remoção de importação de pacotes desnecessários.

--- LayoutGrid/LayoutGrid/LayoutGrid/MainPage.xaml.cs
+++ LayoutGrid/LayoutGrid/LayoutGrid/MainPage.xaml.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics.Contracts;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace LayoutGrid
