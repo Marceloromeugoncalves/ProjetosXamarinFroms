8b67d530b5fdd8a1ffb8b5125fe2047c97303347
Marceloromeugoncalves <marceloromeugoncalves@gmail.com>
2020-11-12 11:06:43 -0300

    Finalização do App Sites.

---
+++ SitesFamosos/SitesFamosos/SitesFamosos/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SitesFamosos.Util;
+using System;
+using Xamarin.Forms;
+
+namespace SitesFamosos
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTapGestureRecognizerTapped1(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri(Links.LinkIconFinder));
+        }
+
+        void OnTapGestureRecognizerTapped2(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri(Links.LinkJsonFormatterValidator));
+        }
+
+        void OnTapGestureRecognizerTapped3(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri(Links.LinkBuildMyString));
+        }
+    }
+}
---
+++ SitesFamosos/SitesFamosos/SitesFamosos/Util/Links.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SitesFamosos.Util
+{
+    public static class Links
+    {
+        public static string LinkIconFinder { get; set; } = "https://www.iconfinder.com/";
+
+        public static string LinkJsonFormatterValidator { get; set; } = "https://jsonformatter.curiousconcept.com/";
+
+        public static string LinkBuildMyString { get; set; } = "https://www.buildmystring.com/";
+    }
+}
