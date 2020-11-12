9209f008a75a24c849657525148f18767698db84
Marceloromeugoncalves <marceloromeugoncalves@gmail.com>
2020-11-12 16:50:47 -0300

    Finalização do projeto navegação entre págians.

---
+++ NavPaginas/NavPaginas/NavPaginas/PageExemplo.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavPaginas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageExemplo : ContentPage
+    {
+        public PageExemplo()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
