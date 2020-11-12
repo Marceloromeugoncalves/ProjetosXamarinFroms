4a5a16f845306543f98120ce78e40221eb46266f
Marceloromeugoncalves <marceloromeugoncalves@gmail.com>
2020-11-12 10:31:13 -0300

    Finalização do AppTabuada.

---
+++ AppTabuada/AppTabuada/AppTabuada/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AppTabuada
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCalcular_Clicked(object sender, EventArgs e)
+        {
+            int numero = 0;
+
+            int.TryParse(entryNumero.Text, out numero);
+
+            List<string> items = new List<string>();
+
+            for (int i = 0; i < 11; i++)
+            {
+                items.Add($"{numero} X {i} = {numero * i}");
+            }
+
+            listViewTabuada.ItemsSource = items;
+        }
+    }
+}
