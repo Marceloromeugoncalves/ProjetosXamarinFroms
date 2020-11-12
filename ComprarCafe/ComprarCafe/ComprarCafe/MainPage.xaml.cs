08b2a0c155b65ef15c66ff45fac4617e8ec05ca1
Marceloromeugoncalves <marceloromeugoncalves@gmail.com>
2020-11-12 10:16:51 -0300

    Finalização do aplicativo Compra de Café.

---
+++ ComprarCafe/ComprarCafe/ComprarCafe/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ComprarCafe
+{
+    public partial class MainPage : ContentPage
+    {
+        int quantidade = 0;
+
+        double valorUnitario = 5;
+
+        double total = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            switch(button.Text)
+            {
+                case "-":
+                    if(quantidade > 0)
+                    {
+                        quantidade--;
+                    }
+                    break;
+
+                case "+":
+                    quantidade++;
+                    break;
+            }
+
+            total = quantidade * valorUnitario;
+
+            labelQuantidade.Text = $"Quantidade: {quantidade}";
+
+            labelTotal.Text = $"Total: R$ {total.ToString("N2")}";
+        }
+    }
+}
