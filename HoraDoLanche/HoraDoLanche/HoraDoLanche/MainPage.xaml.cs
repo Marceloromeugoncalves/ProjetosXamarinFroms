f6ddffa5e8e1f629798e4b8ec2a1d23c5c0667e4
Marceloromeugoncalves <marceloromeugoncalves@gmail.com>
2020-11-12 09:24:27 -0300

    App Hora do Lanche.

---
+++ HoraDoLanche/HoraDoLanche/HoraDoLanche/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace HoraDoLanche
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            button.Text = "Comi o biscoito!";
+
+            foto.Source = "after_cookie";
+        }
+    }
+}
